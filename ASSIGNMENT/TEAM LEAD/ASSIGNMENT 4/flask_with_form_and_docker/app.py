e9b6350d955c88eed9634a4b35f1892b17a921d7
Vethanathan <58129139+Vethanathan@users.noreply.github.com>
2022-11-11 11:44:50 +0530

    change name

--- ASSIGNMENT/TEAM LEAD/ASSIGNMENT 4/flask_with_form_and_docker/app.py
+++ ASSIGNMENT/TEAM LEAD/ASSIGNMENT 4/flask_with_form_and_docker/app.py
@@ -15,7 +15,7 @@ def create():
             flash('Content is required!')
         else:
             messages.append({'title': title, 'content': content})
-            name = "Shyam Mohan"
+            name = "vethanathan V K"
             return redirect(url_for('index', messages=name ))
 
     return render_template('create.html')
