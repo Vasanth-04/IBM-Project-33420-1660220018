ef6b07f666924f4ddaa6ccd22d7b7039f24d1e78
Vethanathan <58129139+Vethanathan@users.noreply.github.com>
2022-11-18 11:58:28 +0530

    sprint pdf

--- Project Development Phase/Sprint 1/app.py
+++ Project Development Phase/Sprint 1/app.py
@@ -8,7 +8,11 @@ import win32api
 #creating an app instance
 app = Flask(__name__)
 
-
+# Project done by
+# VETHANATHAN VK
+# SRIVATSAN KUMAR
+# VIGNESH
+# SHREE KISHORE
 
 
 conn = ibm_db.connect("DATABASE=bludb;HOSTNAME=21fecfd8-47b7-4937-840d-d791d0218660.bs2io90l08kqb1od8lcg.databases.appdomain.cloud;PORT=31864;SECURITY=SSL;SSLSererCertificate=DigiCertGlobalRootCA.crt;UID=nrn19864;PWD=3qwwTpbwWRWevFb6",'','')
